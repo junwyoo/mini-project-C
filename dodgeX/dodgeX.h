ae46b29777440d226396ed2ba7dda9cbcb4e9b01
Junwhan Yoo <junwyoo@hotmail.com>
2018-10-19 11:35:50 +0900

    created headerFile

---
+++ dodgeX/dodgeX.h
@@ -0,0 +1,22 @@
+#ifndef DODGEX_H_
+#define DODGEX_H_
+
+#include <stdlib.h> // srand()
+#include <time.h> // time()
+
+#define ROW 10
+#define COL 40
+#define OBSTACLE 10
+#define ENEMY 3
+#define DIFFICULTY 0 // 0 ~ 2
+
+typedef struct{
+	int x;
+	int y;
+}Position;
+
+int renderEnemy(char* map, Position *enemies, Position user);
+int moveCh(int command, Position* ch, char* map);
+
+
+#endif // #ifndef DODGEX_H_
