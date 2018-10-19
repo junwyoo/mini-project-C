9c38ec3c3c99866b1e715b87127541f37ee20d1e
Junwhan Yoo <junwyoo@hotmail.com>
2018-10-19 15:46:12 +0900

    file io support

--- dodgeX/dodgeX.h
+++ dodgeX/dodgeX.h
@@ -1,6 +1,7 @@
 #ifndef DODGEX_H_
 #define DODGEX_H_
 
+#include <stdio.h>
 #include <stdlib.h> // srand()
 #include <time.h> // time()
 
@@ -8,7 +9,9 @@
 #define COL 40
 #define OBSTACLE 10
 #define ENEMY 3
+#define BONUS 5
 #define DIFFICULTY 0 // 0 ~ 2
+
 
 typedef struct{
 	int x;
@@ -17,6 +20,7 @@ typedef struct{
 
 int renderEnemy(char* map, Position *enemies, Position user);
 int moveCh(int command, Position* ch, char* map);
-
+int loadBest();
+int saveBest(int n);
 
 #endif // #ifndef DODGEX_H_
